bd2ff8ba05a5b402e42d508201cb0c45a679f72a
Nick Babchenko <nik.babchenko@gmail.com>
2019-11-10 13:34:26 +0200

    Adds example with ngIf, ngFor and Click

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -6,7 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
+  title = 'Users App';
+  users = ['Ivan', 'Vadim', 'Igor', 'Denis'];
+  selectedUser = this.users[0];
   constructor() {}
 
   ngOnInit() {}
+
+  onButtonClick(index: number) {
+    this.selectedUser = this.users[index];
+  }
 }
